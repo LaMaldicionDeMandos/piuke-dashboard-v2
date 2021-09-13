eaa39523d2b7a5cc0e515c318f57bfd60d20459c
marcelo <pasutmarcelo@gmail.com>
2021-09-13 09:59:48 -0300

    agrego las sumas de ventas brutas, netas y ganancia

--- src/app/pages/sale/sale.component.ts
+++ src/app/pages/sale/sale.component.ts
@@ -12,6 +12,10 @@ export class SaleComponent implements OnInit {
   startDate = new Date();
   sales: FlattenSale[];
 
+  grossSales: number = 0;
+  netSales: number = 0;
+  profits: number = 0;
+
   constructor(private salesService: SalesService) {
     this.getSales(this.startDate);
   }
@@ -22,7 +26,17 @@ export class SaleComponent implements OnInit {
     this.salesService.getSales(date.getFullYear(), date.getMonth() + 1)
       .then(sales => _.reduce(sales, (s, sale) => _.concat(s, sale.saleItems), []))
       .then(_.reverse)
-      .then(sales => this.sales = sales);
+      .then(sales => this.sales = sales)
+      .then(sales => {
+        this.grossSales = 0;
+        this.netSales = 0;
+        this.profits = 0;
+        _.each(sales, sale => {
+          this.grossSales+= sale.price;
+          this.netSales+= sale.refill;
+          this.profits+= sale.profit;
+        });
+      });
   }
 
   closeDatePicker(eventData: any, dp?:any) {
