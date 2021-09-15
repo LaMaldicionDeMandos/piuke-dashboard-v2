a0936bc40b135b22cd63fe2d00255be7c0146229
marcelo <pasutmarcelo@gmail.com>
2021-09-15 10:04:55 -0300

    trabajo en la maqueta de la pantalla de gastos

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,11 +3,12 @@ import { Routes } from "@angular/router";
 import {StockComponent} from "../../pages/stock/stock.component";
 import {SaleComponent} from "../../pages/sale/sale.component";
 import {BalanceComponent} from "../../pages/balance/balance.component";
+import {ExpensesComponent} from "../../pages/expenses/expenses.component";
 
 export const AdminLayoutRoutes: Routes = [
   { path: "", redirectTo: "stock" },
   { path: "stock", component: StockComponent },
   { path: "sales", component: SaleComponent },
-  { path: "expenses", component: SaleComponent },
+  { path: "expenses", component: ExpensesComponent },
   { path: "balance", component: BalanceComponent }
 ];
--- src/app/pages/expenses/expenses.component.ts
+++ src/app/pages/expenses/expenses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {startLoadingIndicator, stopLoadingIndicator} from "@btapai/ng-loading-indicator";
+
+const emptyExpense = () => { return {desc: '', value: undefined}};
 
 @Component({
   selector: "app-expenses",
@@ -6,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expenses.component.scss']
 })
 export class ExpensesComponent implements OnInit {
+  newItem: any = emptyExpense();
+  showNewExpenseForm = false;
+  startDate = new Date();
+  expenses = [{date: new Date(), desc: 'Cinta de embalar', value: 232.17}];
+
   constructor() {}
 
   ngOnInit() {}
+
+  newExpense() {
+    this.showNewExpenseForm = true;
+  }
+
+  @startLoadingIndicator()
+  save() {
+    this.showNewExpenseForm = false;
+    Promise.resolve()
+      .then(() => {
+        this.triggerLoadingIndicatorStop();
+        this.newItem = emptyExpense();
+      });
+  }
+
+  @stopLoadingIndicator()
+  triggerLoadingIndicatorStop() {
+    console.log('stopped');
+  }
+
+  closeDatePicker(eventData: any, dp?:any) {
+    console.log("Data: " + eventData);
+    this.startDate = eventData;
+    // TODO actualizar gastos this.getSales(this.startDate);
+    dp.close();
+  }
 }
